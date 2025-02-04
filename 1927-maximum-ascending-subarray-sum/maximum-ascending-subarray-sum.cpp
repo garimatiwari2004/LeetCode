5cde78e9f2114b91f4562f76ee4ad90f10ed1c29
Garima Tiwari <109591458+garimatiwari2004@users.noreply.github.com>
2025-02-04 12:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (47.21%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -5,7 +5,10 @@ public:
         int m=nums[0];
        // int sum=0;
         for(int i=1;i<nums.size();++i){
-            c=nums[i]>nums[i-1]?c+nums[i]:nums[i];
+           if(nums[i]>nums[i-1]){
+            c+=nums[i];
+           }
+           else c=nums[i];
             m=max(m,c);
         }
         return m;
