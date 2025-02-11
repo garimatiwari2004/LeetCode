16bc8beb172d560a3516ccd6c3cfd09b6c21678b
Garima Tiwari <109591458+garimatiwari2004@users.noreply.github.com>
2025-02-11 22:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (12.46%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int partLen = part.size();
-        string result;
-        for (char c : s) {
-            result.push_back(c);
-            if (result.size() >= partLen && result.substr(result.size() - partLen) == part) {
-                result.erase(result.size() - partLen, partLen);
-            } }
-        return result;
-    } };
+        string resultStack;
+        int targetLength = part.size();
+        char targetEndChar = part.back();
+
+        for (char currentChar : s) {
+            resultStack.push_back(currentChar);
+
+            if (currentChar == targetEndChar && resultStack.size() >= targetLength) {
+                if (resultStack.substr(resultStack.size() - targetLength) == part) {
+                    resultStack.erase(resultStack.size() - targetLength);
+                }
+            }
+        }
+        return resultStack;
+    }
+};
