77f7c152bddf860b7d7edd0f7949dd9c17f6f43f
Garima Tiwari <109591458+garimatiwari2004@users.noreply.github.com>
2025-02-17 22:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (76.08%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,21 +1,35 @@
-class Solution {
+class Solution { 
 public:
-    std::unordered_set<std::string> all;
-
     int numTilePossibilities(std::string tiles) {
-        std::vector<bool> visited(tiles.size(), false);
-        backtrack(tiles, "", visited);
-        return all.size();
-    }
-
-    void backtrack(std::string& tiles, std::string word, std::vector<bool>& visited) {
-        for (int i = 0; i < tiles.size(); i++) {
-            if (visited[i]) continue;
-            std::string charSequence = word + tiles[i];
-            visited[i] = true;
-            all.insert(charSequence);
-            backtrack(tiles, charSequence, visited);
-            visited[i] = false;
+        int n = tiles.length();
+        std::vector<int> counts(26, 0);
+        std::vector<int> fac(n + 1, 1);
+        for (int i = 1; i <= n; i++) {
+            fac[i] = i * fac[i - 1];
         }
+        for (char c : tiles) {
+            counts[c - 'A']++;
+        }
+        std::vector<int> lengthcounts(n + 1, 0);
+        lengthcounts[0] = 1;
+        for (int i = 0; i < 26; i++) {
+            if (counts[i] > 0) {
+                std::vector<int> temp(n + 1, 0);
+                for (int j = 0; j <= n && lengthcounts[j] > 0; j++) {
+                    for (int k = 1; k <= counts[i]; k++) {
+                        int totallength = j + k;
+                        temp[totallength] += lengthcounts[j] * fac[totallength] / (fac[k] * fac[j]);
+                    }
+                }
+                for (int j = 0; j <= n; j++) {
+                    lengthcounts[j] += temp[j];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += lengthcounts[i];
+        }
+        return ans;
     }
 };
