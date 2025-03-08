9043145a3c70c2fa70bdc45afd448baec33540e3
Garima Tiwari <109591458+garimatiwari2004@users.noreply.github.com>
2025-03-08 23:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 59.4 MB (25.62%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int maxflips=0;
+        int left=0;
+        int count=0;
+        for(int right=0;right<nums.size();++right){
+            if(nums[right]==0){
+                count++;
+            }
+            while(count>k){
+                if(nums[left]==0){
+                    count--;
+                }
+                left++;
+            }
+            maxflips=max(maxflips,right-left+1);
+
+        }
+return maxflips;
+        
+    }
+};
